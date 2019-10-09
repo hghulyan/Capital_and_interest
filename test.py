7c5f5307f5e388643220c92de8cd185ec1bcdbaf
Haik Ghulyan <haikghulyan@Vardans-Air.connect>
2019-10-09 22:34:59 +0100

    Spelling

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ years = int(input("How many years : "))
 count = 1
 cap_1 = capital
 while years >= count:
-    intrest_x = capital * 0.05
-    intrest = capital * 1.05
-    print("intrest",intrest_x)
+    interest_x = capital * 0.05
+    interest = capital * 1.05
+    print("interest",interest_x)
     print("capital",capital)
-    print("total",intrest + capital + intrest_x)
+    print("total",interest + capital + interest_x)
     print(count)
     count = count + 1
     intrest = capital * 1.05
     capital = capital + cap_1
-    whole_year = (intrest + capital + intrest_x) * 12
+    whole_year = (interest + capital + interest_x) * 12
     print("if you have the investment over all 12 months",whole_year)
